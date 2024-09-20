99cc14b7dde52dcb932b84904005f4fea7398502
Yaxuan Li <90280157+COCO0911@users.noreply.github.com>
2024-09-20 13:46:00 -0500

    Time: 0 ms (100.00%) | Memory: 42.3 MB (53.62%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,13 +10,11 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if ( head == null || head.next == null ) return head;
-
         ListNode prev = null;
+
         ListNode cur = head;
 
-        while(cur != null){
-            //this nxt is node 3 when it break
+        while(cur != null) {
             ListNode nxt = cur.next;
             cur.next = prev;
             prev = cur;
@@ -26,3 +24,9 @@ class Solution {
         return prev;
     }
 }
+
+
+
+//  1 <- 2   3 -> 4 -> 5
+
+//                    pre cur nxt
