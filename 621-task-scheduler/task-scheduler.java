10463987256519654d162ef11caa5a98f892fd75
Yaxuan Li <90280157+COCO0911@users.noreply.github.com>
2024-10-06 02:13:34 -0500

    Time: 3 ms (85.14%) | Memory: 46 MB (21.48%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,36 +1,16 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] frequencies = new int[26];
-        for(char i : tasks){
-            frequencies[i - 'A']++; 
+        int[] freq=new int[26];
+        for(char task:tasks){
+            freq[task-'A']++;
         }
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());        //start with most frequent letters
-
-       
-        for(int i = 0; i < 26; i++){
-            if (frequencies[i] > 0) {
-                pq.offer(frequencies[i]);
-            }        
+        Arrays.sort(freq);
+        int maxf=freq[25];
+        int idle=(maxf - 1)*n;
+        for(int i=24;i>=0;i--){
+            idle-=Math.min(freq[i],maxf-1);
         }
-
-        int time = 0;
-        while (!pq.isEmpty()) {
-            int cycle = n + 1;
-            List<Integer> store = new ArrayList<>();
-            int taskCount = 0;
-            while( cycle-- > 0 && !pq.isEmpty()){
-                int curFreq = pq.poll();
-                if (curFreq > 1){
-                    store.add(curFreq - 1);
-                }
-                taskCount++;
-            }
-            store.forEach(pq::offer);
-            // Add time for the completed cycle
-            time += (pq.isEmpty() ? taskCount : n + 1);
-        }
-
-        return time;
+        idle=Math.max(0,idle);
+        return tasks.length+idle;
     }
 }
