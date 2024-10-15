95489370888be396e645fab1a1f26df08bfb10ce
Yaxuan Li <90280157+COCO0911@users.noreply.github.com>
2024-10-15 13:42:54 -0500

    Time: 0 ms (100.00%) | Memory: 41.1 MB (49.61%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -13,16 +13,14 @@
  *     }
  * }
  */
-
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) return null;
+        if (root == null) return null;
         TreeNode left = invertTree(root.left);
         TreeNode right = invertTree(root.right);
-        root.left = right;
         root.right = left;
+        root.left = right;
         
-
         return root;
     }
 }
