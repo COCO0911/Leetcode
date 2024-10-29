5a549e4c4f2062cdbec9903658cbf665fa02f3aa
Yaxuan Li <90280157+COCO0911@users.noreply.github.com>
2024-10-29 16:53:09 -0500

    Time: 6 ms (25.03%) | Memory: 48.8 MB (93.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,43 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        if(grid == null) return 0;
         int m = grid.length;
         int n = grid[0].length;
-        int res = 0;
-
+        int island = 0;
+        boolean[][] visited = new boolean[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1'){
-                  res += dfs(grid, i, j);
-                  
+                if (grid[i][j] == '1' && !visited[i][j]){
+                  bfs(grid, i, j, visited);
+                  island += 1;
                 }
             }
         }
-        return res;
+        return island;
     }
 
-    private int dfs(char[][] grid, int i, int j){
-        if (i < 0 || i >= grid.length ||
-            j < 0 || j >= grid[0].length || grid[i][j] == '0'){
-                return 0;
+    private void bfs(char[][] grid, int i, int j, boolean[][] visited) {        
+        Queue<int[]> q = new ArrayDeque<>();
+        int[][] dirs = {{1, 0},{-1, 0},{0, 1},{0, -1}};
+        visited[i][j] = true;
+        q.offer(new int[]{i, j});
+        while(!q.isEmpty()){
+            int[] current = q.poll();
+            int row = current[0], col = current[1];
+            
+            for (int[] dir : dirs){
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+                
+                // Check boundaries, if it's land ('1') and not visited
+                if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length &&
+                    grid[newRow][newCol] == '1' && !visited[newRow][newCol]) {
+                    q.offer(new int[]{newRow, newCol});
+                    visited[newRow][newCol] = true; 
+                    // mark as visited then we dont visited twice
+                }
             }
-        grid[i][j] = '0';
-        int res = 0;
-        dfs(grid, i - 1, j);
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
-        return 1;
+
+        }  
     }
 }
